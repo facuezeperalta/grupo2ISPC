ab37a02072455026d33721ff3080ce47585c8491
Facuezeperalta <facundoperaltaezequiel@gmail.com>
2022-08-13 22:48:08 -0300

    Cargando ejecricio 4 de Python terminando

---
+++ Ejercicio4_terminado.py
@@ -0,0 +1,12 @@
+﻿# Creo la lista
+Perro2 = [14,"Fido","12/12/2012","Macho",23546987]
+Perro = []
+
+for i in Perro2: #inicio el recorrido de la lista Perro2
+    Perro.insert(0,i) #Inserto los datos en una nueva lista vacia la cual llamé Perro
+    
+Perro 
+print("Datos del Perro2: ", Perro)
+
+
+
